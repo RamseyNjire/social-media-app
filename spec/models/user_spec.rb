fbc2da44690bd9a5a21125babe6a3bc3dec3a30b
Ramsey Njire <ramseynjire@gmail.com>
2021-02-27 12:55:23 +0300

    refactor tests for validating presence and length of various attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before(:all) do
-    ramsey = create(:user)
+  context "validates presence and length of various attributes for a new user" do
+    before(:all) do
+      reson = build(:user)
+    end
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_most(20) }
+    it { should validate_presence_of(:email) }
   end
-
-  it { should validate_presence_of(:name) }
 end
