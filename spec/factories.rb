e46da9bb693df6fb67678841713884f4d02c6d6a
Ramsey Njire <ramseynjire@gmail.com>
2021-02-27 12:38:36 +0300

    got my first test to pass!

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    name { "Ramsey" }
-    email { "ramseynjire@gmail.com" }
+    name { "Reson" }
+    email { "resonnjeri@gmail.com" }
     password { "password" }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,7 +61,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
-  Shoulda::Matchers.configure do
+  Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       # Choose a test framework:
       with.test_framework :rspec
