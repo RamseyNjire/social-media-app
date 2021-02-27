3c79e5baff45828445e40e87a5fcab0c8ae72663
Ramsey Njire <ramseynjire@gmail.com>
2021-02-27 13:28:28 +0300

    Create factory for friendship

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
-  factory :user do
+  factory :user, aliases: [:friend] do
     name { "Reson" }
     email { "reson.njeri@gmail.com" }
     password { "password" }
+  end
+
+  factory :friendship do
+    user { user }
+    friend { user, name: "meteur", email: "meteur@gmail.com" }
+    status { false }
   end
 end
